ee0b908ec80e3ff1aa3b4fb0cee7f8e9be5934d1
william <factzure@hotmail.com>
2021-10-29 16:27:04 -0500

    Agregar archivos de proyecto.

---
+++ CapaDatos/Conexion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
+namespace CapaDatos
+{
+   public class Conexion
+    {
+        string cadena = ConfigurationManager.ConnectionStrings["cnn"].ConnectionString;
+
+       public SqlConnection conectar() {
+            SqlConnection Ocon = new SqlConnection(cadena);
+            return Ocon;
+                }
+
+    }
+}
---
+++ CapaPresentacion/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using CapaDatos;
+namespace CapaPresentacion
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection MiCon;
+            Conexion conectar = new Conexion();
+            MiCon = conectar.conectar();
+            MiCon.Open();
+            MessageBox.Show(MiCon.State.ToString());
+            MiCon.Close();
+
+        }
+    }
+}
